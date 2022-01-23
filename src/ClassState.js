8239f5019e9deabb8874feff68d71a9b58afcc85
wfercanas <fernando.canas@outlook.com>
2022-01-23 12:23:57 -0500

    add/ render UseState and ClassState

---
+++ src/ClassState.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ClassState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Eliminar ClassState</h2>
+        <p>
+          Por favor, escribe el código de seguridad para comprobar que quieres
+          eliminar.
+        </p>
+        <input placeholder='Código de seguridad' />
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
+
+export { ClassState };
---
+++ src/UseState.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>
+        Por favor, escribe el código de seguridad para comprobar que quieres
+        eliminar.
+      </p>
+      <input placeholder='Código de seguridad' />
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState };
