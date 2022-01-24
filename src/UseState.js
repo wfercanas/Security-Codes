067c2fd0e1004af7adb27dd814bbaab536b6a954
wfercanas <fernando.canas@outlook.com>
2022-01-24 10:25:25 -0500

    add/ confirmation and recovery views

--- src/UseState.js
+++ src/UseState.js
@@ -8,15 +8,19 @@ function UseState({ name }) {
     value: '',
     error: false,
     loading: false,
+    confirmation: false,
+    recovery: false,
   });
 
   useEffect(() => {
     if (state.loading) {
       setTimeout(() => {
-        if (!(state.value === SECURITY_CODE)) {
-          setState({ ...state, error: true });
+        if (state.value !== SECURITY_CODE) {
+          console.log('hey!');
+          setState({ ...state, loading: false, error: true });
+        } else {
+          setState({ ...state, loading: false, confirmation: true, value: '' });
         }
-        setState({ ...state, loading: false });
       }, 3000);
     }
   }, [state.loading]);
@@ -27,27 +31,59 @@ function UseState({ name }) {
     }
   }, [state.value]);
 
-  const handleClick = () => {
-    setState({ ...state, error: false, loading: true });
+  const handleConfirmation = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
   };
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>
-        Por favor, escribe el código de seguridad para comprobar que quieres
-        eliminar.
-      </p>
-      {state.error && <p>Error: El código es incorrecto</p>}
-      {state.loading && <p>Cargando...</p>}
-      <input
-        placeholder='Código de seguridad'
-        value={state.value}
-        onChange={({ target }) => setState({ ...state, value: target.value })}
-      />
-      <button onClick={handleClick}>Comprobar</button>
-    </div>
-  );
+  const handleDelete = () => {
+    setState({ ...state, confirmation: false, recovery: true });
+  };
+
+  const handleAbort = () => {
+    setState({ ...state, confirmation: false });
+  };
+
+  const handleRecovery = () => {
+    setState({ ...state, recovery: false });
+  };
+
+  if (!!state.confirmation) {
+    return (
+      <>
+        <p>¿Estás seguro de eliminar Use State?</p>
+        <button onClick={handleDelete}>Sí, eliminar</button>
+        <button onClick={handleAbort}>No, me arrepentí</button>
+      </>
+    );
+  } else if (!!state.recovery) {
+    return (
+      <>
+        <p>Has eliminado Use State</p>
+        <button onClick={handleRecovery}>Recuperar Use State</button>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>
+          Por favor, escribe el código de seguridad para comprobar que quieres
+          eliminar.
+        </p>
+        {!!state.error && <p>Error: El código es incorrecto</p>}
+        {state.loading && <p>Cargando...</p>}
+        <input
+          placeholder='Código de seguridad'
+          value={state.value}
+          onChange={({ target }) => setState({ ...state, value: target.value })}
+        />
+        <button onClick={handleConfirmation}>Comprobar</button>
+      </div>
+    );
+  }
 }
 
 export { UseState };
