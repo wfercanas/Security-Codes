50e96475cfbb6387608be64dfd6bfd561e3877ba
wfercanas <fernando.canas@outlook.com>
2022-01-24 15:57:03 -0500

    add/ Use Reducer component using switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { UseState } from './UseState';
-import { ClassState } from './ClassState';
+import { UseReducer } from './UseReducer';
 
 function App() {
   return (
     <div className='App'>
       <UseState name='Use State' />
-      <ClassState name='Class State' />
+      <UseReducer name='Use Reducer' />
     </div>
   );
 }
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,3 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useReducer } from 'react';
+
+const SECURITY_CODE = 'paradigma';
+
 const initialState = {
   value: '',
   error: false,
@@ -6,26 +11,7 @@ const initialState = {
   recovery: false,
 };
 
-// Forma if-else
 const reducer = (state, action) => {
-  if (action.type === 'ERROR') {
-    return {
-      ...state,
-      error: true,
-      loading: false,
-    };
-  } else if (action.type === 'CHECK') {
-    return {
-      ...state,
-      loading: true,
-    };
-  } else {
-    return { ...state };
-  }
-};
-
-// Forma Switch
-const reducerSwitch = (state, action) => {
   switch (action.type) {
     case 'ERROR':
       return {
@@ -33,23 +19,114 @@ const reducerSwitch = (state, action) => {
         error: true,
         loading: false,
       };
+    case 'CONFIRM':
+      return {
+        ...state,
+        loading: false,
+        confirmation: true,
+        value: '',
+      };
+    case 'TYPING':
+      return {
+        ...state,
+        error: false,
+      };
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.payload,
+      };
+    case 'DELETE':
+      return {
+        ...state,
+        confirmation: false,
+        recovery: true,
+      };
+    case 'ABORT':
+      return {
+        ...state,
+        confirmation: false,
+      };
+    case 'RECOVERY':
+      return {
+        ...state,
+        recovery: false,
+      };
+    case 'CONFIRMATION':
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return { ...state };
   }
 };
 
-// Forma Reducer Object and Function
-const reducerObject = (state) => {
-  return {
-    ERROR: { ...state, error: true, loading: false },
-    CHECK: { ...state, loading: true },
-  };
-};
+function UseReducer({ name }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const reducerFunction = (state, action) => {
-  if (reducerObject(state)[action.type]) {
-    return reducerObject(state)[action.type];
+  useEffect(() => {
+    if (state.loading) {
+      setTimeout(() => {
+        if (state.value !== SECURITY_CODE) {
+          dispatch({ type: 'ERROR' });
+        } else {
+          dispatch({ type: 'CONFIRM' });
+        }
+      }, 3000);
+    }
+  }, [state.loading]);
+
+  useEffect(() => {
+    if (!!state.error) {
+      dispatch({ type: 'TYPING' });
+    }
+  }, [state.value]);
+
+  if (!!state.confirmation) {
+    return (
+      <>
+        <p>¿Estás seguro de eliminar Use State?</p>
+        <button onClick={() => dispatch({ type: 'DELETE' })}>
+          Sí, eliminar
+        </button>
+        <button onClick={() => dispatch({ type: 'ABORT' })}>
+          No, me arrepentí
+        </button>
+      </>
+    );
+  } else if (!!state.recovery) {
+    return (
+      <>
+        <p>Has eliminado Use State</p>
+        <button onClick={() => dispatch({ type: 'RECOVERY' })}>
+          Recuperar Use State
+        </button>
+      </>
+    );
   } else {
-    return state;
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>
+          Por favor, escribe el código de seguridad para comprobar que quieres
+          eliminar.
+        </p>
+        {!!state.error && <p>Error: El código es incorrecto</p>}
+        {state.loading && <p>Cargando...</p>}
+        <input
+          placeholder='Código de seguridad'
+          value={state.value}
+          onChange={({ target }) =>
+            dispatch({ type: 'CHANGE', payload: target.value })
+          }
+        />
+        <button onClick={() => dispatch({ type: 'CONFIRMATION' })}>
+          Comprobar
+        </button>
+      </div>
+    );
   }
-};
+}
+
+export { UseReducer };
