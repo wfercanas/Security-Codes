1f28fd5f5ad56e7880072a2dd74398aeab874dff
wfercanas <fernando.canas@outlook.com>
2022-01-23 19:34:02 -0500

    add/ handleClick and handleChange

--- src/ClassState.js
+++ src/ClassState.js
@@ -2,21 +2,41 @@ import React from 'react';
 
 import { Loading } from './Loading';
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      value: '',
       error: false,
       loading: false,
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidUpdate() {
     if (this.state.loading) {
       setTimeout(() => {
+        if (!(this.state.value === SECURITY_CODE)) {
+          this.setState({ error: true });
+        }
         this.setState({ loading: false });
       }, 3000);
     }
+  }
+
+  handleClick() {
+    this.setState({ error: false, loading: true });
+  }
+
+  handleChange({ target }) {
+    if (!!this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ value: target.value });
   }
 
   render() {
@@ -29,10 +49,12 @@ class ClassState extends React.Component {
         </p>
         {this.state.error && <p>Error: El código es incorrecto</p>}
         {this.state.loading && <Loading />}
-        <input placeholder='Código de seguridad' />
-        <button onClick={() => this.setState({ loading: !this.state.loading })}>
-          Comprobar
-        </button>
+        <input
+          placeholder='Código de seguridad'
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>Comprobar</button>
       </div>
     );
   }
